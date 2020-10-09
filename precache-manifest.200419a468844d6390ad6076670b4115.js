2ff0f4548d2df0cbad44fe9d2cd648bbf4fc28a7
Alexsander <info@lexistudio.ru>
2020-10-09 13:23:30 +0300

    upd

---
+++ precache-manifest.200419a468844d6390ad6076670b4115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19cc1c67513876c3da061d1cbd48e221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3745a481d77d730b3bc1",
+    "url": "/static/css/2.ea4bcfda.chunk.css"
+  },
+  {
+    "revision": "12381346938f6eb0dc48",
+    "url": "/static/css/main.d4a32dde.chunk.css"
+  },
+  {
+    "revision": "3745a481d77d730b3bc1",
+    "url": "/static/js/2.ad2a537e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad2a537e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12381346938f6eb0dc48",
+    "url": "/static/js/main.c7e3ae03.chunk.js"
+  },
+  {
+    "revision": "916d45e11bbfe12864af",
+    "url": "/static/js/runtime-main.94243213.js"
+  }
+]);
